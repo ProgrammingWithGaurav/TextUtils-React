acd9ea095a5e28b5bc8cb28937b08d96542ffe45
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-06 20:57:32 +0530

    adding darkmode

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [text, setText] = useState('Dark Mode'); // Whether dark mode is enabled or not
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+      setText('Light Mode')
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      setText('Dark Mode')
+    }
+  }
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
       {/* <Navbar/> */}
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} text={text}/>
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" />
+        <TextForm heading="Enter the text to analyze below" mode={mode}/>
         {/* <About/> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react'
-import { traverseTwoPhase } from 'react-dom/test-utils';
-
 
 export default function TextForm(props) {
     const handleUpClick = () => {
@@ -40,7 +38,7 @@ export default function TextForm(props) {
         window.speechSynthesis.speak(speech)
     }
 
-    const handleAutoWrite = () => {
+    const handleTalktoType = () => {
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
         const recognition = new SpeechRecognition()
         recognition.onstart = () => {
@@ -61,25 +59,25 @@ export default function TextForm(props) {
     // setText('new Text');  // correct way to change the state
     return (
         <>
-            <div className="container">
+            <div className="container" style={{color: props.mode === 'dark' ? 'white':'black'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="textarea" rows="8" placeholder="Enter your text here"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'? 'grey': 'white', color: props.mode === 'dark' ? 'white':'black'}} id="textarea" rows="8" placeholder="Enter your text here"></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Conver to Uppercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Conver to Lowercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra WhiteSpcaes</button>
-                <button className="btn btn-primary mx-2" onClick={handleReadText}>Read Text</button>
-                <button className="btn btn-primary mx-2" onClick={handleAutoWrite}>Say the Text</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Conver to Uppercase</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Conver to Lowercase</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove Extra WhiteSpcaes</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleReadText}>Read Text</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleTalktoType}>Talk to Type</button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{color: props.mode === 'dark' ? 'white':'black'}}>
                 <h2>Your text summary</h2>
                 <p>{text.split(' ').length} and {text.length} characters</p>
                 <p>{0.008 * text.split(' ').length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:'Enter something in the textbox above to preview it'}</p>
             </div>
         </>
     )
