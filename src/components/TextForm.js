8ce09ae6624678fcec867dd3a291b04ac7a6f0b1
Gaurav <88154142+ProgrammingWithGaurav@users.noreply.github.com>
2021-10-08 20:56:14 +0530

    Update TextForm.js
    fixing more issues

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,11 +22,8 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
-    const handleCopy = () => {
-        const text = document.querySelector('#textarea')
-        text.select()
-        navigator.clipboard.writeText(text.value)
-        document.getSelection().removeAllRanges()
+   const handleCopy = () => {
+        navigator.clipboard.writeText(text)
         props.showAlert('Copied to clipboard!', 'success')
     }
 
@@ -79,7 +76,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-3" style={{color: props.mode === 'dark' ? 'white':'black'}}>
                 <h2>Your text summary</h2>
-                <p>{text.split(' ').filter((element) => {return element.length!==0}).length} and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element) => {return element.length!==0}).length} and {text.length} characters</p>
                 <p>{0.008 * text.split(' ').filter((element) => {return element.length!==0}).length} Minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length>0?text:'Nothing to preview!'}</p>
