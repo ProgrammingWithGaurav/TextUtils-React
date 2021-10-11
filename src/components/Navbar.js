48ed229ced34852698bae0dbed1738f8261b785c
Gaurav <88154142+ProgrammingWithGaurav@users.noreply.github.com>
2021-10-11 11:44:45 +0530

    removing extra themes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,15 +19,9 @@ export default function Navbar(props) {
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/commands">commands</Link>
+              <Link className="nav-link" to="/commands"></Link>
             </li>
           </ul>
-          <div className="d-flex">
-            <div className="bg-primary rounded mx-2" onClick={() => props.toggleMode('primary')} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
-            <div className="bg-danger rounded mx-2" onClick={() => props.toggleMode('danger')} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
-            <div className="bg-success rounded mx-2" onClick={() => props.toggleMode('success')} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
-            <div className="bg-warning rounded mx-2" onClick={() => props.toggleMode('warning')} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
-          </div>
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark': 'light'}`}>
             <input className="form-check-input" onClick={() => props.toggleMode(null)} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.text}</label>
