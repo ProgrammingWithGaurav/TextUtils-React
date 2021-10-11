b7fb875ab4a8c175b8c7cc6260b3a4f5eb27f5d4
Gaurav <88154142+ProgrammingWithGaurav@users.noreply.github.com>
2021-10-11 11:35:48 +0530

    adding more featuers and command buttons
    to use the command button read command in command section

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,7 +22,7 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
-   const handleCopy = () => {
+    const handleCopy = () => {
         navigator.clipboard.writeText(text)
         props.showAlert('Copied to clipboard!', 'success')
     }
@@ -58,6 +58,32 @@ export default function TextForm(props) {
         recognition.start()        
         props.showAlert('say to write text', 'success')
     }
+    const runCommand = () => {
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+        const recognition = new SpeechRecognition()
+        recognition.onstart = () => {
+            console.log('voice is activated, you can to microphone')
+        }
+        
+        recognition.onresult = (event) => {
+            const current = event.resultIndex
+            const transcript = event.results[current][0].transcript
+            console.log(transcript)
+            if(transcript.includes('clear')){
+                handleClearClick()
+            }
+            if(transcript.includes('clear')) handleClearClick()
+            if(transcript.includes('uppercase')) handleUpClick()
+            if(transcript.includes('lowercase')) handleLoClick()
+            if(transcript.includes('copy')) handleCopy()
+            if(transcript.includes('remove whitespace')) handleExtraSpaces()
+            if(transcript.includes('read')) handleReadText()
+            
+            
+        
+        }
+        recognition.start()        
+    }
     const [text, setText] = useState('');
     return (
         <>
@@ -73,11 +99,12 @@ export default function TextForm(props) {
                 <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove Extra WhiteSpcaes</button>
                 <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleReadText}>Read Text</button>
                 <button className="btn btn-primary mx-2 my-1" onClick={handleTalktoType}>Talk to Type</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={runCommand}>Say commands</button>
             </div>
             <div className="container my-3" style={{color: props.mode === 'dark' ? 'white':'black'}}>
                 <h2>Your text summary</h2>
-               <p>{text.split(/\s+/).filter((element) => {return element.length!==0}).length} words and {text.length} characters</p>
-                 <p>{0.008 * text.split(' ').filter((element) => {return element.length!==0}).length} Minutes read</p>
+                <p>{text.split(/\s+/).filter((element) => {return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(' ').filter((element) => {return element.length!==0}).length} Minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length>0?text:'Nothing to preview!'}</p>
             </div>
